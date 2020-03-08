67c3e534eae958ecf4e5a4399e0817c5e40352d8
Erik <2883388711710@ingenieria.usac.edu.gt>
2020-03-07 20:09:53 -0600

    Filtro de pasos

--- src/index.js
+++ src/index.js
@@ -1,65 +1,105 @@
 //Librerias, Modulos y Paquetes
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 //Inicializaciones
 const app = express();
 
 //Configuraciones
-app.set('port',3000 || process.env.PORT);
+app.set("port", 3000 || process.env.PORT);
 app.use(cors());
 
 //Midlewares
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 //DataBase
-require('./db/connection');
-const Record = require('./db/record');
+require("./db/connection");
+const Record = require("./db/record");
 
-var record = new Record({    
-    steps : 0,
-    BPM : 89,
-    weight : 10.3,
-    position : 90,
-    luminousIntensity : 3000,
-    soundIntensity : 100,
-    date : new Date(),
-    water : 900,
-    location : {
-        latitude: 14.607815,
-        longitude : -90.551730,
-        height : 1505
-    }
+var record = new Record({
+  steps: 100,
+  BPM: 89,
+  weight: 10.3,
+  position: 90,
+  luminousIntensity: 3000,
+  soundIntensity: 100,
+  date: new Date("2020-03-03:10:40Z"),
+  water: 900,
+  location: {
+    latitude: 14.607815,
+    longitude: -90.55173,
+    height: 1505
+  }
 });
 
 //Rutas
-app.get('/api/Record',(req,res)=>{
-    Record.find({},(err,records)=>{
-        var recordmap = {};
-        if(err) throw err;
-        records.forEach((record)=>{
-            recordmap[record._id] = record;
-        })
-        res.send(recordmap);
+app.get("/api/Record", (req, res) => {
+  Record.find({}, (err, records) => {
+    var recordmap = {};
+    if (err) throw err;
+    records.forEach(record => {
+      recordmap[record._id] = record;
     });
+    res.send(recordmap);
+  });
 });
 
-app.post('/api/Record',(req,res)=>{
-    record = new Record(req.body);
-    console.log(record);
-    record.save((err,rec)=>{
-        if(err){
-            res.send({status : false});
-            throw err;
-        }
-        console.log(rec);
-        res.send({status : true});
-    });
-})
+app.post("/api/Record", (req, res) => {
+  record = new Record(req.body);
+  console.log(record);
+  record.save((err, rec) => {
+    if (err) {
+      res.send({ status: false });
+      throw err;
+    }
+    console.log(rec);
+    res.send({ status: true });
+  });
+});
 
 //Server
-var server = app.listen(app.get('port'),()=>{
-    console.log('Servidor corriento en el puerto: ' + app.get('port'));
+var server = app.listen(app.get("port"), () => {
+  console.log("Servidor corriento en el puerto: " + app.get("port"));
 });
 
+/*//Pasos por hora, dia, semana
+Record.find({"date" : {"$gte" : start, "$lte" : end}})
+    .select('date steps')
+    .exec((err,records)=>{
+    if(err)
+        throw err;
+    console.log(records);
+});
+*/
+var start = new Date('2020-03-03:00:00Z');
+var end = new Date('2020-03-20T23:59:59Z');
+
+function pasosFiltro(start, end,filtro){
+  Record.aggregate(
+    [
+      {
+        $match: { date: { $gte: start, $lte : end} }
+      },
+      {
+        $group: {
+          _id: { $dateToString: { format: filtro, date: "$date" } },
+          totalStep: {
+            $sum: "$steps"
+          },
+        }
+      },
+      {
+        $sort: { _id: 1 }
+      }
+    ],
+    (err, res) => {
+      if (err) throw err;
+      console.log(res);
+    }
+  );
+}
+
+pasosFiltro(start,end,'%Y-%m-%d');
+
+pasosFiltro(start,end,'%Y-%m-%d %H');
